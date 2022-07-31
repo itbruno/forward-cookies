9a101f6fd99f5622369031932cbd270def7ddee0
Bruno Rodrigues <brunowebr@gmail.com>
2022-07-31 20:00:17 -0300

    Add some instance to enable lib methods

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { ForwardCookies } from "./lib/forwardCookies";
+
+var pageCookie = new ForwardCookies("user", true);
+
+var btnGet = document.querySelector(".js-get-cookie");
+var btnAdd = document.querySelector(".js-add-cookie");
+var btnSetUrl = document.querySelector(".js-seturl-cookie");
+var btnDelete = document.querySelector(".js-delete-cookie");
+
+btnGet?.addEventListener("click", () => {
+  pageCookie.getCookie();
+});
+
+btnAdd?.addEventListener("click", () => {
+  pageCookie.setCookie({
+    cookieName: "user",
+    value: String(Math.random()),
+  });
+});
+
+btnSetUrl?.addEventListener("click", () => {
+  pageCookie.generateCookieAsUrl();
+});
+
+btnDelete?.addEventListener("click", () => {
+  pageCookie.deleteCookie();
+});
+
+console.log(pageCookie.getCookieFromUrl());
